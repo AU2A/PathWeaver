761fe80cc05183e9134e0298c442945245992aff
Aura <aura.cs12@nycu.edu.tw>
2024-07-31 13:31:28 +0800

    feat: save output to file

--- main.py
+++ main.py
@@ -2,28 +2,40 @@ import argparse, os
 
 
 class printFolder:
-    def __init__(self, path):
-        self.path = os.path.normpath(path)
-        print(f"{self.path}/")
-        self.printFolderTree(path, " ")
+    def __init__(self, args):
+        self.path = args.path
+        self.output = f"{os.path.basename(os.path.dirname(self.path))}/"
+        self.printFolderTree(args.path, " ")
+        with open(args.output, "w") as f:
+            f.write(self.output)
 
     def printFolderTree(self, path, prefix):
         if len(os.listdir(path)) == 0:
             return
         lastFile = os.listdir(path)[-1]
         for file in os.listdir(path):
-            print(prefix, end="")
+            self.output += "\n" + prefix
             if os.path.isdir(os.path.join(path, file)):
-                print(f"{"┗" if file == lastFile else "┣"}━ {file}/")
+                if file == lastFile:
+                    self.output += f"┗━ {file}/"
+                else:
+                    self.output += f"┣━ {file}/"
                 self.printFolderTree(
-                    os.path.join(path, file), prefix + ("    " if file == lastFile else "┃   ")
+                    os.path.join(path, file),
+                    prefix + ("    " if file == lastFile else "┃   "),
                 )
             else:
-                print(f"{"┗" if file == lastFile else "┣"}━ {file}")
+                if file == lastFile:
+                    self.output += f"┗━ {file}"
+                else:
+                    self.output += f"┣━ {file}"
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Path where you want to create.", type=str)
+    parser.add_argument(
+        "-o", "--output", help="Output file name.", type=str, default="output.txt"
+    )
     args = parser.parse_args()
-    printFolder(args.path)
+    printFolder(args)
